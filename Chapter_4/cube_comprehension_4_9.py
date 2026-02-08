edcbe0d570d167a10f5bfbd7e0273660f3e9657e
Michael Moore <moorem022758@gmail.com>
2026-02-08 10:59:06 -0600

    Adding 3 new files to chapter 4 folder lessons

---
+++ Chapter_4/cube_comprehension_4_9.py
@@ -0,0 +1,4 @@
+# Cube comprehension
+# use a list comprehension to generate a list of the first 10 cubes
+cubes = [value**3 for value in range(1, 10)]
+print(cubes)
---
+++ Chapter_4/cubes_4_8.py
@@ -0,0 +1,11 @@
+# a number raised to the third power is called a cube. Fir example, the cuve of 2 is written
+# as 2 ** 3 in python.
+# Make a list of the first 10 cubes (that is, the cube of each integer from 1 through 10),
+# and use a for loop to print the valus of each cute.
+cubes = []
+for cube in range(1, 10):
+    cube1 = cube ** 3
+    cubes.append(cube1)   
+print(cubes)
+    
+    
