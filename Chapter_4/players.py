2b272e8e21d4bc4d1f92f11a11c42bad851665cc
Michael Moore <moorem022758@gmail.com>
2026-02-08 12:04:44 -0600

    Adding one more file to chapter 4

---
+++ Chapter_4/players.py
@@ -0,0 +1,10 @@
+# slicing a list
+players = ["charles", "martina", "michael", "florence", "eli"]
+# print(players[0:3])
+# print(players[1:4])
+# print(players[:4])
+# print(players[2:])
+# print(players[-3:])
+print("Here are the first three players on our team")
+for player in players[:3]:
+    print(player.title())
