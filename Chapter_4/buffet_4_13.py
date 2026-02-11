d32ec17d7a1afe0746f91873f7497368c6a89139
Michael Moore <moorem022758@gmail.com>
2026-02-10 19:39:10 -0600

    adding the final 3 files to complete Chapter 4 work

---
+++ Chapter_4/buffet_4_13.py
@@ -0,0 +1,10 @@
+# Think of five simple foods, and store them in a tuple.
+# Use a for loop to print each food the restaurant offers 
+                                         
+buffet_foods = ("steak", "chicken", "fish", "lobster", "crab")
+for buffet_food in buffet_foods:
+    print(buffet_food)
+    
+buffet_foods = ("steak", "chicken", "fish", "sushi", "eggrolls")
+for buffet_food in buffet_foods:
+    print(buffet_food)
---
+++ Chapter_4/code_review_4_15.py
@@ -0,0 +1,27 @@
+# Code Review
+
+# Tuples
+
+dimensions = (200, 50)
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+    
+dimensions = (400, 100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
+    
+# print(dimensions[0])
+# print(dimensions[1])
+
+# Think of five simple foods, and store them in a tuple.
+# Use a for loop to print each food the restaurant offers 
+                                         
+buffet_foods = ("steak", "chicken", "fish", "lobster", "crab")
+for buffet_food in buffet_foods:
+    print(buffet_food)
+    
+buffet_foods = ("steak", "chicken", "fish", "sushi", "eggrolls")
+for buffet_food in buffet_foods:
+    print(buffet_food)
