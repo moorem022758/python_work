ed5ed48ef15262c49b08803b4d3f8cfdf2905cc3
Michael Moore <moorem022758@gmail.com>
2026-02-15 15:13:41 -0600

    Adding new file to chapter 6 2/15/2026

---
+++ Chapter_6/alien.py
@@ -0,0 +1,46 @@
+# A simple dictionary
+
+# alien_0 = {"color": "green", "points": "5"}
+# print(alien_0 ["color"])
+# print(alien_0 ["points"])
+
+# new_points = alien_0["points"]
+# print(f'You just earned {new_points}')
+
+# print(alien_0)
+# alien_0["x_position"] = 0
+# alien_0["y_position"] = 25
+# print(alien_0)
+
+# alien_0 = {}
+# alien_0['color'] = "green"
+# alien_0["points"] = "5"
+# print(alien_0)
+
+# alien_0 = {"color" : "green"}
+# print(f"The alien is {alien_0['color']}")
+
+# alien_0["color"] = "yellow"
+# print(f"The alien is now {alien_0['color']}")
+
+# alien_0 = {"x_position" : 0, "y_position" : 25}
+# print(f"Original position: {alien_0 ['x_position']}")
+
+# alien_0 ["speed"] = "medium"
+
+# if alien_0["speed"] == "slow":
+    # x_increment = 1
+# elif alien_0["speed"] == "medium":
+    # x_increment = 2
+# else:
+    # x_increment = 3
+    
+# alien_0 ["x_position"] = alien_0["x_position"] + x_increment
+# print(f"New position: {alien_0 ['x_position']}")
+
+alien_0 = {"color" : "green", "points" : "5"}
+print(alien_0)
+
+del alien_0 ["points"]
+print(alien_0)
+    
