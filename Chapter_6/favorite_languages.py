de0029a97a51787e4022efef83b8c1617c3f7919
Michael Moore <moorem022758@gmail.com>
2026-02-16 21:15:03 -0600

    update for this first file in chapter 6 2/16/2026

--- Chapter_6/favorite_languages.py
+++ Chapter_6/favorite_languages.py
@@ -1,5 +1,6 @@
 # A Dictionary of Similar Objects
 
+# This separates dictionary items lines
 favorite_languages = {
     "jen" : "python",
     "sarah" : "c",
@@ -7,7 +8,11 @@ favorite_languages = {
     "phil" : "python",
 }
 
+
+# This prints information in the dicitionary
 print(favorite_languages)
 
+
+# This creates a new variable to hold the key for sarah and then prints it
 language = favorite_languages["sarah"].title()
 print(f"Sarah's favorite language is {language}")
