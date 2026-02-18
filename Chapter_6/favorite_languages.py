7d9c9191930b5a51ec440b8b42d2b4aa9565bd43
Michael Moore <moorem022758@gmail.com>
2026-02-18 15:37:28 -0600

    adding more files to Chapter 6 2/18/2026

--- Chapter_6/favorite_languages.py
+++ Chapter_6/favorite_languages.py
@@ -10,9 +10,28 @@ favorite_languages = {
 
 
 # This prints information in the dicitionary
-print(favorite_languages)
+# print(favorite_languages)
 
 
 # This creates a new variable to hold the key for sarah and then prints it
-language = favorite_languages["sarah"].title()
-print(f"Sarah's favorite language is {language}")
+# language = favorite_languages["sarah"].title()
+# print(f"Sarah's favorite language is {language}")
+
+# For loop to print each person's favorite language
+# for name, language_1 in favorite_languages.items():
+    # print(f"{name.title()}'s favorite language is {language_1}")
+    
+# for name in favorite_languages.keys():
+    # 
+    # print(name.title())
+    
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}.")
+    
+if name in friends:
+        language = favorite_languages[name].title()               
+        print(f"\t{name.title()}, I see you love {language} !")
+
+if 'erin' not in favorite_languages.keys():
+    print ("Erin, please take our poll!")
---
+++ Chapter_6/user.py
@@ -0,0 +1,14 @@
+# Looping Through a Dictionary
+
+# Looping Through All Key-Value Pairs
+user_0 = {
+    "username" : "efermi",
+    "first" : "enrico",
+    "last" : "fermi",
+}
+
+# Looping through dictionary using a For Loop
+for Key, Value in user_0.items():
+    print(f"\nKey: {Key}")
+    print(f"Value: {Value}")
+    
