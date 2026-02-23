10c3e7a6ed7fb21af6f8753d067b248c3385f3a7
Michael Moore <moorem022758@gmail.com>
2026-02-23 16:08:35 -0600

    last file for chapter 6 2/23/2026

--- Chapter_6/extensions_6_12.py
+++ Chapter_6/extensions_6_12.py
@@ -5,18 +5,24 @@
 cities = {
     'chattanooga' : {
      'fact' : 'choo choo',
+     'state' : 'tennessee',
+     'college' : 'university of tennessee at chattanooga',
      'population' : '30,000,000', 
      'country' : 'united states',
     },
     
     'montgomery' : {
         'fact' : 'civil rights',
+        'state' : 'alabama',
+        'college' : 'alabama state university',
         'population' : '25,000,000',
         'country' : 'united states',
     },
     
     'valdosta' : {
         'fact' : 'vidalia onions',
+        'state' : 'georgia',
+        'college' : 'valdosta state university',
         'population' : '38,000,000',
         'country' : 'united states',
     },
@@ -25,10 +31,14 @@ cities = {
 # Loop through dictionary and print key and value info
 
 for people_info, user_info in cities.items():
-    # print(f'\npeople: {people_info}')
+    print(f'\nCity Locations and Facts: {people_info}')
     facts = user_info['fact']
+    states = user_info['state']
+    colleges = user_info['college']
     populations = user_info['population']
     countries = user_info['country']
     print(f"\tFact: {facts.title()}")
+    print(f"\tState: {states.title()}")
+    print(f"\tCollege: {colleges.title()}")
     print(f"\tPopulation: {populations.title()}")
     print(f"\tCountries: {countries.title()}")
