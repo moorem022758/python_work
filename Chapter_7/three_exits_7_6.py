8c4e4dfa606aabc82cf9ed275e5071f7a52b5846
Michael Moore <moorem022758@gmail.com>
2026-02-28 18:18:12 -0600

    new files to Chapter 7 2/28/20026

---
+++ Chapter_7/three_exits_7_6.py
@@ -0,0 +1,30 @@
+# Three Exits 
+
+
+
+# pizza_topping = "\nPlease Enter your Pizza Toppings: " # Prompt the user to enter pizza toppings
+# pizza_topping += "\n(Enter 'quit' when you are done)" # Add instructions for quitting the loop
+
+# while True:                           # Start an infinite loop
+    # toppings = input(pizza_topping)   #
+    
+    # if toppings == 'quit':            # If the user enters 'quit', exit the loop
+        # break # Exit the loop 
+    # else:       # If the user enters a topping, print a message confirming the addition of the topping to the pizza
+        # print(f'\nI will add your {toppings.title()} topping to your pizza.')
+                
+# Conditional test in while loop to stop the loop
+
+# pizza = input('Enter the number 3: ')
+# pizza = int(pizza)
+
+# while pizza != 3:
+    # break
+    
+
+# Infinite loop
+
+x = 1
+
+while x <= 5:
+    print(x)
