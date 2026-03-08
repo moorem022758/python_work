ab61a643fbf19724f84b61d797288e64fe74651a
Michael Moore <moorem022758@gmail.com>
2026-03-08 18:48:08 -0500

    More Chapter 8 files 3/8/2026

---
+++ Chapter_8/city_names_8_6.py
@@ -0,0 +1,7 @@
+# City Names 
+
+# write a function called city_country()
+def city_country(city_name, country_name):
+    print(f"\n{city_name.title()}, {country_name.title()}")
+    
+city_country("chattanooga", "United States")
--- Chapter_8/greeter.py
+++ Chapter_8/greeter.py
@@ -1,7 +1,28 @@
 # Defining a Function
 
-def greet_user(username):
-    """Display a simple greeting."""
-    print(f"Hello {username.title()} !")
+# def greet_user(username):
+    # """Display a simple greeting."""
+    # print(f"Hello {username.title()} !")
     
-greet_user('Micheal Moore')
+# greet_user('Micheal Moore')
+
+# Using a function with a while loop
+
+def get_formatted_name(first_name, last_name):
+    """Return a fullname, neatly formatted."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+# This is the infinite loop!
+while True:
+    print("\nPlease tell me your name:")
+    print("(enter 'q' at any time to quit)")
+    f_name = input("First name: ")
+    if f_name == 'q':
+        break
+    l_name = input("Last name: ")
+    if l_name == 'q':
+        break
+    
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name} !")
