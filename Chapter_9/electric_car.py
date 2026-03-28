308f20c563979b5c0749b8e77f102a1c23e4d55c
Michael Moore <moorem022758@gmail.com>
2026-03-27 20:59:47 -0500

    updating an existing file in chapter 9 3/27/2026

--- Chapter_9/electric_car.py
+++ Chapter_9/electric_car.py
@@ -30,6 +30,10 @@ class Car:
         """Add the given amount to the odometer reading."""
         self.odometer_reading += miles
         
+    def fill_gas_tank(self):
+        """Simulate filling the gas tank."""
+        print("The gas tank is now full.")  
+        
         
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
@@ -37,13 +41,30 @@ class ElectricCar(Car):
     def __init__(self, make, model, year):
         """Initialize attributes of the parent class."""
         super().__init__(make, model, year)
-        self.battery_size = 40
+        self.battery = Battery()
         
     def describe_battery(self):
         """Print a statement describing the battery size."""
-        print(f"This car has a {self.battery_size}-kWh battery.") 
+        print(f"This car has a {self.battery}-kWh battery.") 
+        
+    def fill_gas_tank(self):
+        """Electric cars don't have a gas tank"""
+        print("This car doesn't have a gas tank!")
+        
+class Battery:
+    """A simple attempt to model a battery for an Electric Car."""
+    
+    def __init__(self, battery_size=40):
+        """Initialize the battery's attributes"""
+        self.battery_size = battery_size
+        
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f'This car has a {self.battery_size}-kWh battery.')
         
         
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
-my_leaf.describe_battery()
+my_leaf.battery.describe_battery()
+my_leaf.fill_gas_tank()
+my_leaf.battery.describe_battery()
