9af3e751e7e2626e97989fba1b80d185a512397d
Michael Moore <moorem022758@gmail.com>
2026-04-08 21:29:12 -0500

    Update files for chapter 9 4/8/2026

--- Chapter_9/my_car.py
+++ Chapter_9/my_car.py
@@ -3,7 +3,18 @@
 
 """A set of classes used to represent gas and electric cars."""
 # from car import Car, ElectricCar
+
+"""This imports the module"""
 # import car
+
+"""This imports all Classes from a module"""
+from car import * 
+
+"""This imports class using an Aliases"""
+from electric_car import ElectricCar as EC
+
+"""This imports the module and uses Aliases"""
+import electric_car as ec
 
 
 # my_new_car = Car('audi', 'a4', 2024)
@@ -13,9 +24,17 @@
 # my_new_car.read_odometer()
 
 # my_mustang = Car('ford', 'mustang', 2024)
-my_mustang = car.Car('ford', 'mustang', 2024)
+# my_mustang = car.Car('ford', 'mustang', 2024)
+my_mustang = Car('ford', 'mustang', 2024)
 print(my_mustang.get_descriptive_name())
 
 # my_leaf = ElectricCar('nissan', 'leaf', 2024)
-my_leaf = car.ElectricCar('nissan', 'leaf', 2024)
+# my_leaf = car.ElectricCar('nissan', 'leaf', 2024)
+my_leaf = ElectricCar('nissan', 'leaf', 2024)
+print(my_leaf.get_descriptive_name())
+
+my_leaf = EC('nissan', 'leaf', 2026)
+print(my_leaf.get_descriptive_name())
+
+my_leaf = ec.ElectricCar('nissan', 'leaf', 2027)
 print(my_leaf.get_descriptive_name())
